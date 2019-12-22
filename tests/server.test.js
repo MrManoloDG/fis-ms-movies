30c0c783707aef5cd873e4cd8ddec4a21e971a13
JsAntoPe <jsperegrina1@gmail.com>
2019-12-22 12:24:00 +0000

    Moving the movie model into a directory, for some refactoring

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,5 @@
 const api = require('../index.js');
-const movie = require('../movie');
+const movie = require('../models/movie');
 const BASE_API_PATH = "/api/v1";
 const Movie_Api = "/movies_status";
 const supertest = require('supertest');
