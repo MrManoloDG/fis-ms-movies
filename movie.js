fd228b4b72d4d73b714b8dcbfc0ef329dee99e82
Manuel <ManuelDiazGil96@hotmail.com>
2019-12-23 17:18:49 +0100

    Revert "Required fields for movie_status"
    This reverts commit 68bc5f7eae209a5d6aa9ccf07990caed6ab18901.

--- movie.js
+++ movie.js
@@ -2,22 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let movieSchema = Schema({
-    id_user: {
-        type: String, 
-        required:true
-    },
-    id_movie: {
-        type: String, 
-        required:true
-    },
-    status:	{
-        type: String, 
-        required:true
-    },
-    status_date: {
-        type: Date,
-        required: true
-    }
+    _id: String,
+    id_user: String,
+    id_movie: String,
+    status:	String
 });
 
 let movie = mongoose.model('movie', movieSchema);
