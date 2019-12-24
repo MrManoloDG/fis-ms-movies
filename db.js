eece53819c616517147f7b9f2aff01aa3bb29bee
Manuel <ManuelDiazGil96@hotmail.com>
2019-12-24 20:00:06 +0100

    Deploy changes

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const DB_URL = (process.env.MONGO_URL || 'mongodb://mongo:27017');
 const dbConnect = function() {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
-    return mongoose.connect(DB_URL, { useNewUrlParser: true });
+    return mongoose.connect(DB_URL, {useUnifiedTopology: true,useNewUrlParser: true});
 }
 
 module.exports = dbConnect;
