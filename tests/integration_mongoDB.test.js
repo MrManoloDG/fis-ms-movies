6ac2f3b0473ef40490229e094a136f7b2478e3ce
JsAntoPe <jsperegrina1@gmail.com>
2019-12-28 19:56:05 +0000

    One integration test added

--- tests/integration_mongoDB.test.js
+++ tests/integration_mongoDB.test.js
@@ -8,14 +8,29 @@ describe('MongoDB integration tests', () => {
     });
 
     beforeEach((done) => {
-        movie.deleteMany({}, (err) => {
+        Movie.deleteMany({}, (err) => {
             done();
         });
     });
 
     it("Writes a movie in DB", (done) => {
-        const movie = new Movie({});
-        done();
+        const movie = new Movie(
+            {
+                "id_user": "Juanito",
+                "id_movie": "abc2",
+                "status": "Completed",
+                "status_date": new Date(),
+                "genre": ["Action", "Adventure"]
+            }
+        );
+
+        Movie.create(movie, (err) => {
+            expect(err).toBeNull();
+            Movie.find({}, (err, movies) => {
+                expect(movies).toBeArrayOfSize(1);
+                done();
+            });
+        });
     });
 
     afterAll((done) => {
