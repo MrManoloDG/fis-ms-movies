8276a1ea00ad79225c72c37e01294d105f6b4d7d
JsAntoPe <jsperegrina1@gmail.com>
2019-12-30 20:20:48 +0000

    Checking the correctness of the four basical operations on MongoDB

--- tests/integration_mongoDB.test.js
+++ tests/integration_mongoDB.test.js
@@ -13,6 +13,13 @@ describe('MongoDB integration tests', () => {
         });
     });
 
+    it("Checks the database is empty", (done) => {
+        Movie.find({}, (err, movies) => {
+            expect(movies).toBeArrayOfSize(0);
+            done();
+        });
+    });
+
     it("Writes a movie in DB", (done) => {
         const movie = new Movie(
             {
@@ -30,6 +37,34 @@ describe('MongoDB integration tests', () => {
                 expect(movies).toBeArrayOfSize(1);
                 done();
             });
+        });
+    });
+
+    it("Checking update works", (done) => {
+        movie = new Movie(
+            {
+                "id_user": "Juanito",
+                "id_movie": "abc2",
+                "status": "Completed",
+                "status_date": new Date()
+                //"genre": ["Action", "Adventure"]
+            }
+        );
+
+        Movie.create(movie, (err) => {
+            expect(err).toBeNull();            
+            Movie.updateOne({_id: movie._id}, {status: "Pending"}, (err, nrep) => {
+                expect(err).toBeNull();
+                expect(nrep).toStrictEqual({"n": 1, "nModified": 1, "ok": 1});
+                done();
+            });
+        });
+    });
+
+    it("Checks the database is empty at the end of the tests, proving the delete works", (done) => {
+        Movie.find({}, (err, movies) => {
+            expect(movies).toBeArrayOfSize(0);
+            done();
         });
     });
 
