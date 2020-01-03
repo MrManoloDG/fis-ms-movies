baf61df252096071dbe11f664d7cfbc543cd840a
Manuel <frkwtf@gmail.com>
2020-01-03 19:25:04 +0100

    Show the db url to connect

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const DB_URL = (process.env.MONGO_URL || 'mongodb://mongo:27017');
 
 const dbConnect = function() {
+    console.log("Trying to connect: " + DB_URL);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
     return mongoose.connect(DB_URL, { useNewUrlParser: true });
